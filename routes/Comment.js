08b17ce954cb18d406848eb32dcc00cb9c660496
Ashutosh Mangal <ashutoshmangal99@gmail.com>
2023-01-15 23:02:44 +0530

    initial commits

---
+++ routes/Comment.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import {
+  addComment,
+  deleteComment,
+  editComment,
+} from "../controllers/Comment.js";
+import auth from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.post("/", addComment);
+// router.get("/get", getAllQuestions);
+router.delete("/delete/:questionId/:questionCommentId", deleteComment);
+router.patch("/edit/:questionId/:questionCommentId", editComment);
+
+export default router;
